ff4c510b9795ffaf32ace485980a9c71ac572831
BrianCibrian <109054870+BrianCibrian@users.noreply.github.com>
2024-06-21 01:48:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 console.log("Script running");
 
+const userApiKey = prompt(
+  "Please paste your Giphy API key here.  See the lesson notes for instructions on how to get one.  The code will not work without this."
+);
+
 // Helper function - gets a random integer up to (but not including) the maximum
 const getRandomInt = (max) => {
   return Math.floor(Math.random() * Math.floor(max));
@@ -15,8 +19,24 @@ console.log(submitButton);
 console.log(queryField);
 console.log(imageHolderDiv);
 
-submitButton.addEventListener("click", (e) => {
-  let myKey = "YOUR_API_KEY_HERE";
+submitButton.addEventListener("click", async (e) => {
+  let myKey = userApiKey;
   let topic = queryField.value;
-  console.log(topic);
+  let myQuery = `https://api.giphy.com/v1/gifs/search?api_key=${myKey}&q=${topic}+kitten`;
+  console.log(myQuery);
+
+  // fetch(...).then(() => {...}).catch(() => {...});
+  try {
+    const response = await fetch(myQuery);
+    const responseJson = await response.json(); // read JSON response
+
+    // code to execute once JSON response is available
+    let i = getRandomInt(10);
+    let imgurl = responseJson.data[i].images.downsized.url;
+    console.log(imgurl);
+    imageHolderDiv.innerHTML =
+      `<img src="${imgurl}"/>` + imageHolderDiv.innerHTML;
+  } catch (error) {
+    console.error(error);
+  }
 });
